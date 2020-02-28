51586b10a6813df2e002299aa46be4dab115cf7e
Muhammad Adeel <smaa.link@gmail.com>
2020-02-28 10:41:54 +0430

    remove favicon.ic

---
+++ client/build/precache-manifest.46e65a087dbbd176c64f9efe982f9e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b7abd7dfa8c8f556dee38dcb788508",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4723fa2be2e33c62cd68",
+    "url": "/static/css/2.ce2441d0.chunk.css"
+  },
+  {
+    "revision": "4723fa2be2e33c62cd68",
+    "url": "/static/js/2.aa085522.chunk.js"
+  },
+  {
+    "revision": "ea4b5ae141dc8c71e175204f6e8e684d",
+    "url": "/static/js/2.aa085522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ef87c5ada8ac093df5",
+    "url": "/static/js/main.e430be7a.chunk.js"
+  },
+  {
+    "revision": "4ce1e997fd5995525ed5",
+    "url": "/static/js/runtime-main.906551b1.js"
+  }
+]);
